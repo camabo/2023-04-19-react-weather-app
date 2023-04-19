a5240781ffa9218eef99df4a75b20d96f672cfb0
camabo <125750117+camabo@users.noreply.github.com>
2023-04-19 09:10:39 -0600

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Caitlin Bolanos and is
-        <a
-          href="https://github.com/camabo/2023-04-19-react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Caitlin Bolanos and is
+          <a
+            href="https://github.com/camabo/2023-04-19-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
