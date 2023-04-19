900859cc832f24049b73020e28521ca38d63fc81
camabo <125750117+camabo@users.noreply.github.com>
2023-04-19 08:59:00 -0600

    Added the Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Caitlin Bolanos and is
+        <a
+          href="https://github.com/camabo/2023-04-19-react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
