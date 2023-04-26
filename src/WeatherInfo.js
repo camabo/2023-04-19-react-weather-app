39430ccf987c8a70e4fa2d7eeb95709f842f367d
camabo <125750117+camabo@users.noreply.github.com>
2023-04-26 10:06:47 -0600

    fixed css for icon and temp WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,29 +5,29 @@ export default function WeatherInfo(props) {
   return (
     <div className="current-forecast-section">
       <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
       <div className="row">
         <div className="col-6 d-flex align-items-center">
-          <ul>
-            <li>
-              <FormattedDate date={props.data.date} />
-            </li>
-            <li className="text-capitalize">{props.data.description}</li>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)}km/h</li>
-          </ul>
-        </div>
-        <div className="col-6 text-center">
           <img
             className="current-icon"
             src={props.data.iconUrl}
             alt={props.data.icon}
           />
-          <div>
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </div>
+          <span className="temperature">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6 d-flex align-items-center">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)}km/h</li>
+          </ul>
         </div>
       </div>
     </div>
