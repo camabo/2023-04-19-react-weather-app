c0cd0e5896740219fb4c0b52bce982b635907944
camabo <125750117+camabo@users.noreply.github.com>
2023-04-26 14:31:11 -0600

    Designed layout for forecast section

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./WeatherForecast.css";
+import WeatherInfo from "./WeatherInfo";
+import Weather from "./Weather";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
@@ -9,7 +11,7 @@ export default function WeatherForecast() {
           <div className="WeatherForecast-day">Thursday</div>
           <div>
             {" "}
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>{" "}
+            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
           </div>
           <span className="WeatherForecast-temperature-max">10°C</span>
           <span className="WeatherForecast-temperature-min">15°C</span>
